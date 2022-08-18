9cf220aa68f3562ea3784ee0b176cbcec44fe2a6
Sanesh S <106676454+SaneshSabu@users.noreply.github.com>
2022-08-18 18:43:07 +0530

    Create variable.tf
    Variables

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "cidr" {
+  default = "172.19.0.0/16"
+}
+
+variable "project" {
+  default = "Sample_Project"
+}
+
+variable "env" {
+  default = "Developmet"
+}
+
+variable "ami" {
+    default = "ami-076e3a557efe1aa9c"
+}
+
+variable "type" {
+    default = "t2.micro"
+}
+
